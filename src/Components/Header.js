ec946734cf757b39d8a90396247ab7d13039a929
Lei Chen <leichen@leis-mbp.lan>
2023-10-19 20:14:17 -0500

    feat: update header section
    Update header section.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,7 +17,7 @@ class Header extends Component {
           <li><a className="smoothscroll" href='#about' onClick={this.props.aboutHandler}>About</a></li>
 	        <li><a className="smoothscroll" href='#resume' onClick={this.props.resumeHandler}>Resume</a></li>
           <li><a className="smoothscroll" href="#projects" onClick={this.props.projectHandler}>Projects</a></li>
-          <li><a className="smoothscroll" href="#food" onClick={this.props.foodHandler}>Food</a></li>
+          {/* <li><a className="smoothscroll" href="#food" onClick={this.props.foodHandler}>Food</a></li> */}
             
           {/* <li><a className="smoothscroll" href="#contact" onClick={this.props.contactHandler}>Contact</a></li> */}
         </ul>
@@ -25,37 +25,37 @@ class Header extends Component {
       </nav>
 
       <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">Paul Lee</h1>
-            <h3>
-               <span>A <b>Passionate</b> Software Engineer</span>
-            </h3>
-            <hr />
-            <ul className="social">
+        <div className="banner-text">
+          <h1 className="responsive-headline">Lei Chen</h1>
+          <h3>
+             <span>A <b>Passionate</b> Software Engineer</span>
+          </h3>
+          {/* <hr /> */}
+          <ul className="social">
 
-               <li key="medium">
-                  <a href="https://medium.com/@eellaup">
-                     <i className="fab fa-medium"></i>
-                  </a>
-               </li>
+            {/* <li key="medium">
+              <a href="https://medium.com/@eellaup">
+                <i className="fab fa-medium"></i>
+              </a>
+            </li> */}
 
-               <li key="linkedin">
-                  <a href="https://www.linkedin.com/in/eellaup/">
-                     <i className="fab fa-linkedin"></i>
-                  </a>
-               </li>
+            <li key="linkedin">
+              <a href="https://www.linkedin.com/in/lei-chen-820a953b/">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </li>
 
-               <li key="github">
-                  <a href="https://github.com/Eellaup">
-                     <i className="fab fa-github"></i>
-                  </a>
-               </li>
-            </ul>
-         </div>
+            <li key="github">
+              <a href="https://github.com/graffaner/">
+                <i className="fab fa-github"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
 
       <p className="scrolldown">
-         <a className="smoothscroll" href="#aboutBrief"><i className="icon-down-circle"></i></a>
+        <a className="smoothscroll" href="#aboutBrief"><i className="icon-down-circle"></i></a>
       </p>
 
       </header>
