411d473dcf78a2e7225f18a2020072de4cd1022c
Lei Chen <leichen@leis-mbp.lan>
2023-10-19 20:00:17 -0500

    feat: change home page
    Change home page caption.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,18 +9,18 @@ class Header extends Component {
 
       <nav id="nav-wrap" className="headerNav">
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home" onClick={this.props.frontpageHandler}>Home</a></li>
-            <li><a className="smoothscroll" href='#about' onClick={this.props.aboutHandler}>About</a></li>
-	         <li><a className="smoothscroll" href='#resume' onClick={this.props.resumeHandler}>Resume</a></li>
-            <li><a className="smoothscroll" href="#projects" onClick={this.props.projectHandler}>Projects</a></li>
-            <li><a className="smoothscroll" href="#food" onClick={this.props.foodHandler}>Food</a></li>
+        <ul id="nav" className="nav">
+          <li className="current"><a className="smoothscroll" href="#home" onClick={this.props.frontpageHandler}>Home</a></li>
+          <li><a className="smoothscroll" href='#about' onClick={this.props.aboutHandler}>About</a></li>
+	        <li><a className="smoothscroll" href='#resume' onClick={this.props.resumeHandler}>Resume</a></li>
+          <li><a className="smoothscroll" href="#projects" onClick={this.props.projectHandler}>Projects</a></li>
+          <li><a className="smoothscroll" href="#food" onClick={this.props.foodHandler}>Food</a></li>
             
-            {/* <li><a className="smoothscroll" href="#contact" onClick={this.props.contactHandler}>Contact</a></li> */}
-         </ul>
+          {/* <li><a className="smoothscroll" href="#contact" onClick={this.props.contactHandler}>Contact</a></li> */}
+        </ul>
 
       </nav>
 
@@ -28,7 +28,7 @@ class Header extends Component {
          <div className="banner-text">
             <h1 className="responsive-headline">Paul Lee</h1>
             <h3>
-               An Austin-based <span><s>Mechanical</s> Software Engineer</span>
+               <span>A <b>Passionate</b> Software Engineer</span>
             </h3>
             <hr />
             <ul className="social">
