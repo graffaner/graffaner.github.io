ebd90ee5a61510e889d09649eba46c8f3b36ac89
Lei Chen <leichen@leis-mbp.lan>
2023-10-23 22:09:23 -0500

    feat: update project page
    Update project page.

--- src/Components/Projects/ProjectTabs.js
+++ src/Components/Projects/ProjectTabs.js
@@ -1,88 +1,94 @@
 import React, {Component} from 'react';
-
 import projectData from './projectData';
 
 class Tabs extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            activeTab: 0,
-            data: projectData
-        }
+    this.state = {
+        activeTab: 0,
+        data: projectData
+    };
 
-        this.changeTabOnClick = this.changeTabOnClick.bind(this);
-    }
+    this.changeTabOnClick = this.changeTabOnClick.bind(this);
+  }
 
-    // Change tabs on click
-    changeTabOnClick(index) {
-        this.setState({
-            activeTab: index
-        });
-    }
+  // Change tabs on click
+  changeTabOnClick(index) {
+    this.setState({
+      activeTab: index
+    });
+  }
 
-    render() {
-        return (
-            <div className="tabs-body">
-                {/* Tab Header */}
-                <TabHeader data={this.state.data}
-                              click={this.changeTabOnClick}
-                              activeId={this.state.activeTab} />
-                {/* Tab Data */}
-                <TabContent data={this.state.data}
-                               activeId={this.state.activeTab} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="tabs-body">
+        {/* Tab Header */}
+        <TabHeader
+          data={this.state.data}
+          click={this.changeTabOnClick}
+          activeId={this.state.activeTab}
+        />
+        {/* Tab Data */}
+        <TabContent
+          data={this.state.data}
+          activeId={this.state.activeTab}
+        />
+      </div>
+    )
+  }
 }
 
-class TabHeader extends React.Component {
-    doClick(index, event) {
-        this.props.click(index);
-    }
+class TabHeader extends Component {
+  doClick(index, event) {
+    this.props.click(index);
+  }
 
-    render() {
-        let activeClass = this.props.activeId;
+  render() {
+    let activeClass = this.props.activeId;
 
-        let tabs = this.props.data.map((item, index) => {
-            return <li className={(activeClass === index ? 'active' : '')}>
-                        <a onClick={this.doClick.bind(this, index)} >
-                            {/* The Span tilts the boxes */}
-                            <span>
-                                {/* div puts a new line (icon above title) */}
-                                <div>
-                                    <i className={item.icon}></i>
-                                </div>
-                                {item.name}
-                            </span>
-                        </a>
-                    </li>
-        });
+    let tabs = this.props.data.map((item, index) => {
+      const listClassName = (activeClass === index ? 'active' : '');
+      return (
+        <li className={listClassName}>
+          <a onClick={this.doClick.bind(this, index)} >
+            {/* The Span tilts the boxes */}
+            <span>
+              {/* div puts a new line (icon above title) */}
+              <div>
+                <i className={item.icon}></i>
+              </div>
+              {item.name}
+            </span>
+          </a>
+        </li>
+      );
+    });
 
-        return (
-            <ul className="tabs-header">{tabs}</ul>
-        )
-    }
-
+    return (
+      <ul className="tabs-header">{tabs}</ul>
+    );
+  }
 }
 
 class TabContent extends React.Component {
-    render() {
-        let activeClass = this.props.activeId;
+  render() {
+    let activeClass = this.props.activeId;
 
-        let content = this.props.data.map((item, index) => {
-            return (
-                <div className={'tabs-textItem ' + (activeClass === index ? 'show' : '')} >
-                    {/* <p>{item.text}</p> */}
-                    {item.text}
-                </div>
-            )
-        });
+    let content = this.props.data.map((item, index) => {
+      const itemClassName = 'tabs-textItem ' + (activeClass === index ? 'show' : '');
+      return (
+        <div className={itemClassName}>
+          {/* <p>{item.text}</p> */}
+          {item.text}
+        </div>
+      )
+    });
 
-        return (
-            <div className="tabs-content">{content}</div>
-        );
-    }
+    return (
+      <div className="tabs-content">{content}</div>
+    );
+  }
 }
 
 export default Tabs;
