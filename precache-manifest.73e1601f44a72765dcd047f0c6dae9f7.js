d2152a2912b6ae9916d06e6f85d6807545cc3ad1
Lei Chen <leichen@leis-mbp.lan>
2023-10-25 15:54:44 -0500

    Updates

---
+++ precache-manifest.73e1601f44a72765dcd047f0c6dae9f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba53405b9086d02beb5b2b260443c7a",
+    "url": "/leichen_web/index.html"
+  },
+  {
+    "revision": "dd174ae5f3dc29d2b4ec",
+    "url": "/leichen_web/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2cf7788e4bf3ba47af25",
+    "url": "/leichen_web/static/js/2.8d87c3c6.chunk.js"
+  },
+  {
+    "revision": "88a1e5fde3fbde07a43dac26e09075cb",
+    "url": "/leichen_web/static/js/2.8d87c3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd174ae5f3dc29d2b4ec",
+    "url": "/leichen_web/static/js/main.b746edcd.chunk.js"
+  },
+  {
+    "revision": "a8732e10b5af37cd985d",
+    "url": "/leichen_web/static/js/runtime-main.35f8ca20.js"
+  }
+]);
